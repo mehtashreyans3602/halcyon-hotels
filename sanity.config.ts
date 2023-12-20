7378070e9d915767a24f46737e50ba53abdffefc
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2023-12-20 17:01:55 +0530

    Added Sanity Code and Moved files to Web Code
    Moved page.tsx, layout.tsx, favicon.ico and globals.css in (web) folder.

---
+++ sanity.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'halcyon-hotels',
+
+  projectId: process.env.NEXT_PUBLIC_SANITY_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
+  basePath:"/studio",
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
